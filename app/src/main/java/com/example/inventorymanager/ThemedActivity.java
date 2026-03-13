0dcab91c477d45785b88068fc8b1dc9487b95d91
rkghr <rkghrud@gmail.com>
2026-03-13 13:48:53 +0900

    Refresh themed screens after palette changes

--- app/src/main/java/com/example/inventorymanager/ThemedActivity.java
+++ app/src/main/java/com/example/inventorymanager/ThemedActivity.java
@@ -8,15 +8,33 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
 
 public abstract class ThemedActivity extends AppCompatActivity {
+    private boolean appliedDarkModeEnabled;
+    private int appliedThemeResId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        appliedDarkModeEnabled = SettingsStore.isDarkModeEnabled(this);
+        appliedThemeResId = SettingsStore.resolveThemeResId(this);
         AppCompatDelegate.setDefaultNightMode(
-                SettingsStore.isDarkModeEnabled(this)
+                appliedDarkModeEnabled
                         ? AppCompatDelegate.MODE_NIGHT_YES
                         : AppCompatDelegate.MODE_NIGHT_NO
         );
-        setTheme(SettingsStore.resolveThemeResId(this));
+        setTheme(appliedThemeResId);
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        boolean currentDarkModeEnabled = SettingsStore.isDarkModeEnabled(this);
+        int currentThemeResId = SettingsStore.resolveThemeResId(this);
+        if (appliedDarkModeEnabled != currentDarkModeEnabled || appliedThemeResId != currentThemeResId) {
+            appliedDarkModeEnabled = currentDarkModeEnabled;
+            appliedThemeResId = currentThemeResId;
+            recreate();
+        }
     }
 
     protected int resolveThemeColor(int attrResId) {
